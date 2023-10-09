016c1dad41547a72375852d7cc0fcdfa81935cd6
Macheddie <macheddiebaker@gmail.com>
2023-10-08 20:00:16 -0500

    Main updated

--- eco395m-MidtermProject/code/main.py
+++ eco395m-MidtermProject/code/main.py
@@ -7,7 +7,7 @@ IN_PATH = os.path.join("data", "Heart_Disease_Mortality_Data_Among_US_Adults__35
 
 if __name__ == "__main__":
     # df = pd.read_csv(IN_PATH)
-    df = pd.read_csv('eco395m-MidtermProject/data/Heart_Disease_Mortality_Data_Among_US_Adults__35___by_State_Territory_and_County.csv')
+    df = pd.read_csv('../data/Heart_Disease_Mortality_Data_Among_US_Adults__35___by_State_Territory_and_County.csv')
     pnw_states = ["WA", "OR", "ID", "MT", "CA"]
     filtered_df = df[df['LocationAbbr'].isin(pnw_states)]
     # print(filtered_df)
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     base_url = 'https://ridb.recreation.gov/api/v1/'
     endpoint = 'facilities'
     api_key = '4adb7c65-557b-483e-b06b-035223c1c5a2'
-    params = {'state': pnw_states, 'limit': 20, 'offset': 0}
+    params = {'state': pnw_states, 'limit': 10, 'offset': 0}
     response = requests.get(base_url+endpoint, params=params, headers={'apikey': api_key})
     response_json = response.json()
     print(json.dumps(response_json, indent=4))
