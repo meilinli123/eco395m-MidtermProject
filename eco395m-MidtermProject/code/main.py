cd1cc078c79ae7fc50c396e5ea024795875eac6c
Macheddie <macheddiebaker@gmail.com>
2023-10-08 19:50:19 -0500

    ingesting API data

--- eco395m-MidtermProject/code/main.py
+++ eco395m-MidtermProject/code/main.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
-
+import requests
+import json
 
 IN_PATH = os.path.join("data", "Heart_Disease_Mortality_Data_Among_US_Adults__35___by_State_Territory_and_County.csv")
 
@@ -12,3 +13,12 @@ if __name__ == "__main__":
     # print(filtered_df)
     # unique_values = filtered_df['LocationID'].unique()
     # print(unique_values)
+
+
+    base_url = 'https://ridb.recreation.gov/api/v1/'
+    endpoint = 'facilities'
+    api_key = '4adb7c65-557b-483e-b06b-035223c1c5a2'
+    params = {'state': pnw_states, 'limit': 20, 'offset': 0}
+    response = requests.get(base_url+endpoint, params=params, headers={'apikey': api_key})
+    response_json = response.json()
+    print(json.dumps(response_json, indent=4))
