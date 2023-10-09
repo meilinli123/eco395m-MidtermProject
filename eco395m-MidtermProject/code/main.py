f47c1458e7d0f9b6c9d80c57145f64041930cd05
Macheddie <macheddiebaker@gmail.com>
2023-10-09 13:20:57 -0500

    API Passing Long and Lat to state

--- eco395m-MidtermProject/code/main.py
+++ eco395m-MidtermProject/code/main.py
@@ -2,6 +2,24 @@ import os
 import pandas as pd
 import requests
 import json
+import geopy
+from geopy.geocoders import Nominatim, Photon
+
+#pt 1. to get state info based on longitude and Latitude from the rec website:
+
+import requests
+
+def get_country(lat, lon):
+    url = f'https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=json&accept-language=en&zoom=5'
+    try:
+        result = requests.get(url=url)
+        result_json = result.json()
+        return result_json['display_name']
+    except:
+        return None
+
+
+#pt 2. ingest Heart Disease Data
 
 IN_PATH = os.path.join("data", "Heart_Disease_Mortality_Data_Among_US_Adults__35___by_State_Territory_and_County.csv")
 
@@ -14,11 +32,45 @@ if __name__ == "__main__":
     # unique_values = filtered_df['LocationID'].unique()
     # print(unique_values)
 
+#pt3. ingest recreational data
 
     base_url = 'https://ridb.recreation.gov/api/v1/'
     endpoint = 'facilities'
     api_key = '4adb7c65-557b-483e-b06b-035223c1c5a2'
-    params = {'state': pnw_states, 'limit': 10, 'offset': 0}
+    params = {'state': pnw_states, 'limit': 5, 'offset': 0}
     response = requests.get(base_url+endpoint, params=params, headers={'apikey': api_key})
     response_json = response.json()
-    print(json.dumps(response_json, indent=4))
+    df_recdata = pd.DataFrame(response_json["RECDATA"])
+
+    print(df_recdata.columns)
+    print(df_recdata["GEOJSON"][2])
+    #print(response_json["METADATA"]["RESULTS"]["CURRENT_COUNT"])
+    #print(json.dumps(response_json, indent=4)["METADATA"]["RESULTS"]["CURRENT_COUNT"])
+
+    print(get_country(33,-96)) #this is just an example I entered - it prints Texas, US
+
+
+#pt 4. create new col - states based on long and lat - merge two data frames on like col
+
+
+def get_state(location):
+    lat, lon = map(float, location.strip('()').split(', '))
+    url = f'https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format=json&accept-language=en&zoom=5'
+    try:
+        result = requests.get(url=url)
+        result_json = result.json()
+        address = result_json.get('address', {})
+        state = address.get('state', None)
+        return state
+    except:
+        return None
+
+#Applying the get_state function to create a 'State' column in both dataframes
+df['State'] = df['Location 1'].apply(lambda location: get_state(location))
+df_recdata['State'] = df_recdata.apply(lambda row: get_state(row['FacilityLatitude'], row['FacilityLongitude']), axis=1)
+
+# Now that both dataframes have a 'State' column based on latitude and longitude Merge the two dataframes based on 'State' column
+merged_df = pd.merge(df, df_recdata, on='State', how='inner')
+
+print(merged_df.head())
+merged_df.to_csv('merged_data.csv', index=False)
