a682b64173e678084c52d86c4688b8f3c21e2995
nanchic <qxm@wireless-10-145-21-20.public.utexas.edu>
2023-10-11 17:45:19 -0500

    clean code in histgram_pretty.py

--- code/histgram_pretty.py
+++ code/histgram_pretty.py
@@ -18,13 +18,16 @@ ax1 = plt.subplot(2, 1, 1)
 
 for i, activity in enumerate(activities_of_interest):
     participation_rate = data[activity]
-    ax1.bar(x_positions + i * 0.2 - 0.3, participation_rate, width=0.2, label=activity, color=activity_colors[i])
+    ax1.bar(x_positions + i * 0.2, participation_rate, width=0.2, label=activity, color=activity_colors[i])
 
 ax1.set_xlabel('State')
 ax1.set_ylabel('Participation Rate')
 ax1.set_title('Participation Rate of Activities by State')
-ax1.set_xticks(x_positions - 0.2)
+ax1.set_xticks(x_positions + 0.2)
 ax1.set_xticklabels(unique_states, rotation=45)  # Rotate state labels for better visibility
+
+for i, state in enumerate(unique_states):
+    ax1.text(i + 0.2, -10, state, ha='center')
 
 ax1.legend()
 ax1.grid(True, linestyle='--', alpha=0.5)  # Add grid lines with a dashed style and reduced opacity
@@ -40,8 +43,11 @@ ax2.set_title('Mortality Rate by State')
 ax2.set_xticks(x_positions)
 ax2.set_xticklabels(unique_states, rotation=45)  # Rotate state labels for better visibility
 
+for i, state in enumerate(unique_states):
+    ax2.text(i, -10, state, ha='center')
+
 ax2.grid(True, linestyle='--', alpha=0.5)
 
 plt.tight_layout()
-plt.savefig(os.path.join(BASE_DIR, "histogram2.png"))
+
 plt.show()
